c706a8398958ab35f42c5482da0d9845e7e3e730
ow <volovykolya@gmail.com>
2019-02-11 17:19:49 -0800

    fix bugs

--- Program.cs
+++ Program.cs
@@ -1,33 +1,29 @@
 using System;
 
 
-  public class Program
+  class Program
   {
+    public static bool IsPalindrome(string str)
+    {
+    int i = 0;
+    int j = str.Length - 1;
+
+    while (i<j)
+    {
+        if (str[i] != str[j])
+            return false;
+        i++;
+        j--;
+    }
+    return true;
+    }
     public static void Main()
     {
       Console.WriteLine("PALINDROM");
       Console.WriteLine("Enter a word/number/sentance: ");
       string userInput = Console.ReadLine();
 
-      bool IsPalindrome(string str)
-      {
-      int i = 0;
-      int j = str.Length - 1;
-
-      while (i<j)
-      {
-          if (str[i] != str[j])
-              return false;
-              Console.WriteLine("false");
-
-          i++;
-          j--;
-      }
-      return true;
-      Console.WriteLine("true");
-      }
-
-      void IsPalindrome(userInput);
+      Console.WriteLine(IsPalindrome(userInput));
 
     }
   }
